ee042babdf4c8a9909f5a36758286ccc7a9af04f
Josua Schmid <josua.schmid@renuo.ch>
2016-12-07 16:02:02 +0100

    Add Layout with listing of one gathering

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Meetsense.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "None"
   end
 end
