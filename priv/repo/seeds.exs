7ef80e3ed2e3834e12f5b4c124efcfeec9ab4459
TimoSci <me@timosci.net>
2016-12-07 13:46:20 +0100

    finish seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -30,32 +30,40 @@ Repo.insert!(%Gathering {name: "Bowling"})
 Repo.insert!(%Gathering {name: "Dinner"})
 Repo.insert!(%Gathering {name: "Birthday Party"})
 
-# # Repo.delete_all UserGathering
-#
-# Repo.insert!(%UserGathering {user_id: 1, gathering_id: 1})
-# Repo.insert!(%UserGathering {user_id: 2, gathering_id: 1})
-# Repo.insert!(%UserGathering {user_id: 3, gathering_id: 1})
-# Repo.insert!(%UserGathering {user_id: 4, gathering_id: 1})
-#
-# # Repo.delete_all Votable
-#
-# Repo.insert!(%Votable {name: 'Monday', gathering_id: 1})
-# Repo.insert!(%Votable {name: 'Tuesday', gathering_id: 1})
-# Repo.insert!(%Votable {name: 'Wednesday', gathering_id: 1})
-# Repo.insert!(%Votable {name: 'Thursday', gathering_id: 1})
-# Repo.insert!(%Votable {name: 'Friday', gathering_id: 1})
-#
-# # Repo.delete_all Vote
-#
-# Repo.insert!(%Vote {votable_id: 1, user_id: 1, value: 1})
-# Repo.insert!(%Vote {votable_id: 1, user_id: 2, value: 1})
-# Repo.insert!(%Vote {votable_id: 1, user_id: 3, value: 0})
-# Repo.insert!(%Vote {votable_id: 2, user_id: 1, value: 1})
-# Repo.insert!(%Vote {votable_id: 2, user_id: 2, value: -1})
-# Repo.insert!(%Vote {votable_id: 2, user_id: 3, value: 0})
-# Repo.insert!(%Vote {votable_id: 3, user_id: 1, value: 0})
-# Repo.insert!(%Vote {votable_id: 3, user_id: 2, value: 0})
-# Repo.insert!(%Vote {votable_id: 3, user_id: 3, value: -1})
-# Repo.insert!(%Vote {votable_id: 3, user_id: 1, value: 0})
-# Repo.insert!(%Vote {votable_id: 3, user_id: 2, value: -1})
-# Repo.insert!(%Vote {votable_id: 3, user_id: 3, value: -1})
+# Repo.delete_all UserGathering
+
+Repo.insert!(%UserGathering {user_id: 1, gathering_id: 1})
+Repo.insert!(%UserGathering {user_id: 2, gathering_id: 1})
+Repo.insert!(%UserGathering {user_id: 3, gathering_id: 1})
+Repo.insert!(%UserGathering {user_id: 4, gathering_id: 1})
+
+# Repo.delete_all Votable
+
+Repo.insert!(%Votable {name: "Monday", gathering_id: 1})
+Repo.insert!(%Votable {name: "Tuesday", gathering_id: 1})
+Repo.insert!(%Votable {name: "Wednesday", gathering_id: 1})
+Repo.insert!(%Votable {name: "Thursday", gathering_id: 1})
+Repo.insert!(%Votable {name: "Friday", gathering_id: 1})
+
+# Repo.delete_all Vote
+
+Repo.insert!(%Vote {votable_id: 1, user_id: 1, value: 1})
+Repo.insert!(%Vote {votable_id: 1, user_id: 2, value: 1})
+Repo.insert!(%Vote {votable_id: 1, user_id: 3, value: 0})
+Repo.insert!(%Vote {votable_id: 1, user_id: 4, value: 0})
+Repo.insert!(%Vote {votable_id: 2, user_id: 1, value: 1})
+Repo.insert!(%Vote {votable_id: 2, user_id: 2, value: -1})
+Repo.insert!(%Vote {votable_id: 2, user_id: 3, value: 0})
+Repo.insert!(%Vote {votable_id: 2, user_id: 4, value: 1})
+Repo.insert!(%Vote {votable_id: 3, user_id: 1, value: 0})
+Repo.insert!(%Vote {votable_id: 3, user_id: 2, value: 0})
+Repo.insert!(%Vote {votable_id: 3, user_id: 3, value: -1})
+Repo.insert!(%Vote {votable_id: 3, user_id: 4, value: 1})
+Repo.insert!(%Vote {votable_id: 4, user_id: 1, value: -1})
+Repo.insert!(%Vote {votable_id: 4, user_id: 2, value: 1})
+Repo.insert!(%Vote {votable_id: 4, user_id: 3, value: -1})
+Repo.insert!(%Vote {votable_id: 4, user_id: 4, value: -1})
+Repo.insert!(%Vote {votable_id: 5, user_id: 1, value: 0})
+Repo.insert!(%Vote {votable_id: 5, user_id: 2, value: -1})
+Repo.insert!(%Vote {votable_id: 5, user_id: 3, value: -1})
+Repo.insert!(%Vote {votable_id: 5, user_id: 4, value: -1})
