c1041f3f4fa87d88551c97f15cc42a844091be87
TimoSci <me@timosci.net>
2016-12-07 12:21:38 +0100

    create seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -17,11 +17,45 @@ alias Meetsense.Gathering
 alias Meetsense.Votable
 alias Meetsense.Vote
 
-Repo.insert!(%User {name: 'timo'})
-Repo.insert!(%User {name: 'josua'})
-Repo.insert!(%User {name: 'code monkey'})
+# Repo.delete_all User
 
-Repo.insert!(%Gathering {name: 'Bowling'})
-Repo.insert!(%Gathering {name: 'Bowling'})
-Repo.insert!(%Gathering {name: 'Bowling'})
+Repo.insert!(%User {name: "timo"})
+Repo.insert!(%User {name: "josua"})
+Repo.insert!(%User {name: "code monkey"})
+Repo.insert!(%User {name: "jim"})
 
+# Repo.delete_all Gathering
+
+Repo.insert!(%Gathering {name: "Bowling"})
+Repo.insert!(%Gathering {name: "Dinner"})
+Repo.insert!(%Gathering {name: "Birthday Party"})
+
+# # Repo.delete_all UserGathering
+#
+# Repo.insert!(%UserGathering {user_id: 1, gathering_id: 1})
+# Repo.insert!(%UserGathering {user_id: 2, gathering_id: 1})
+# Repo.insert!(%UserGathering {user_id: 3, gathering_id: 1})
+# Repo.insert!(%UserGathering {user_id: 4, gathering_id: 1})
+#
+# # Repo.delete_all Votable
+#
+# Repo.insert!(%Votable {name: 'Monday', gathering_id: 1})
+# Repo.insert!(%Votable {name: 'Tuesday', gathering_id: 1})
+# Repo.insert!(%Votable {name: 'Wednesday', gathering_id: 1})
+# Repo.insert!(%Votable {name: 'Thursday', gathering_id: 1})
+# Repo.insert!(%Votable {name: 'Friday', gathering_id: 1})
+#
+# # Repo.delete_all Vote
+#
+# Repo.insert!(%Vote {votable_id: 1, user_id: 1, value: 1})
+# Repo.insert!(%Vote {votable_id: 1, user_id: 2, value: 1})
+# Repo.insert!(%Vote {votable_id: 1, user_id: 3, value: 0})
+# Repo.insert!(%Vote {votable_id: 2, user_id: 1, value: 1})
+# Repo.insert!(%Vote {votable_id: 2, user_id: 2, value: -1})
+# Repo.insert!(%Vote {votable_id: 2, user_id: 3, value: 0})
+# Repo.insert!(%Vote {votable_id: 3, user_id: 1, value: 0})
+# Repo.insert!(%Vote {votable_id: 3, user_id: 2, value: 0})
+# Repo.insert!(%Vote {votable_id: 3, user_id: 3, value: -1})
+# Repo.insert!(%Vote {votable_id: 3, user_id: 1, value: 0})
+# Repo.insert!(%Vote {votable_id: 3, user_id: 2, value: -1})
+# Repo.insert!(%Vote {votable_id: 3, user_id: 3, value: -1})
