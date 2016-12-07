5ed86140dd8eb8490e1098dada2ae604c165e222
TimoSci <me@timosci.net>
2016-12-07 16:24:15 +0100

    functions for votable scores

--- web/models/votable.ex
+++ web/models/votable.ex
@@ -18,4 +18,21 @@ defmodule Meetsense.Votable do
     |> cast(params, [:name, :description])
     |> validate_required([:name, :description])
   end
+
+
+  #TODO DRY below
+
+  def total_score(id) do
+    votes(id) |> Meetsense.Repo.all |> Enum.sum
+  end
+
+  def vote_count(id,value) do
+    query = from v in "votables", where: v.id == ^id, join: vote in "votes", on: vote.votable_id == v.id, select: vote.value, where: vote.value == ^value
+    Meetsense.Repo.all(query) |> Enum.count
+  end
+
+  def votes(id) do
+    from v in "votables", where: v.id == ^id, join: vote in "votes", on: vote.votable_id == v.id, select: vote.value
+  end
+
 end
