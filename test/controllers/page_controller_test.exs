13185bf6b018d9ab6db147b695c2361d884c200b
Josua Schmid <josua.schmid@renuo.ch>
2016-12-07 16:05:32 +0100

    Also test the listing

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,20 @@
 defmodule Meetsense.PageControllerTest do
   use Meetsense.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "shows an empty page if uninitialized", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "None"
+    assert html_response(conn, 200) =~ "There are not gatherings yet"
+  end
+
+  test "shows a gathering, the associated users and the votables", %{conn: conn} do
+    gathering = Repo.insert! %Meetsense.Gathering{name: "Fishing"}
+    user = Repo.insert! %Meetsense.User{name: "Peter"}
+    Repo.insert! %Meetsense.UserGathering{user: user, gathering: gathering}
+    votable = Repo.insert! %Meetsense.Votable{name: "November 1st", gathering: gathering}
+
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ gathering.name
+    assert html_response(conn, 200) =~ user.name
+    assert html_response(conn, 200) =~ votable.name
   end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,7 +4,7 @@ defmodule Meetsense.PageController do
   def index(conn, _params) do
     case first_gathering do
       nil ->
-        render conn, "index.html", gathering: %Meetsense.Gathering {name: 'None'}, users: [], votables: []
+        render conn, "empty.html"
       _ ->
         gathering = gathering_with_associations(first_gathering)
         render conn, "index.html", gathering: gathering, users: gathering.users, votables: gathering.votables
