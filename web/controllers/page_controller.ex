a2a8344aac5da36684ab3615d66e7b2395883d87
TimoSci <me@timosci.net>
2016-12-09 23:34:23 +0100

    one page for each gathering

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,18 +1,30 @@
+require IEx
+
 defmodule Meetsense.PageController do
   use Meetsense.Web, :controller
 
-  def index(conn, _params) do
+  alias Meetsense.Vote
+  alias Meetsense.Gathering
+
+  def index(conn, params) do
     case first_gathering do
       nil ->
         render conn, "empty.html"
       _ ->
-        gathering = gathering_with_associations(first_gathering)
+        vote_changeset = Vote.changeset(%Vote{})
+        # gathering = gathering_with_associations(first_gathering)
+        gathering = gathering_with_associations(gathering(params["gathering_id"]))
         render conn, "index.html", gathering: gathering, users: gathering.users, votables: gathering.votables
     end
   end
 
   defp first_gathering do
     Meetsense.Gathering |> Ecto.Query.first |> Repo.one
+  end
+
+  defp gathering(id) do
+    # Meetsense.Gathering |> Ecto.Query.where(id: 1) |> Meetsense.Repo.one
+    Repo.get!(Gathering, id)
   end
 
   defp gathering_with_associations(gathering) do
